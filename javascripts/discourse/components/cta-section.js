3b413d4ee84ffb66c5f7e5106005bbcd4ac90120
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-06-23 16:09:32 -0700

    inital commit

---
+++ javascripts/discourse/components/cta-section.js
@@ -0,0 +1,31 @@
+import Component from "@ember/component";
+import discourseComputed from "discourse-common/utils/decorators";
+import { inject as service } from "@ember/service";
+import { computed } from "@ember/object";
+import { defaultHomepage } from "discourse/lib/utilities";
+export default Component.extend({
+  router: service(),
+
+  @discourseComputed("currentUser")
+  hideLoggedIn(currentUser) {
+    return (
+      (currentUser ? true : false)
+    );
+  },
+
+
+  @discourseComputed("router.currentRouteName", "router.currentURL")
+  showHere(currentRouteName, currentURL) {
+    if (settings.show_on === "all") {
+      return true;
+    }
+
+    if (settings.show_on === "discovery") {
+      return currentRouteName.indexOf("discovery") > -1;
+    }
+
+    if (settings.show_on === "homepage") {
+      return currentRouteName == `discovery.${defaultHomepage()}`;
+    }
+  },
+});
